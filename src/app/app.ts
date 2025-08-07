b5fc86e33dae6c69f7e67f96cf04a6dbaf5b8512
DavidSoaresRamos <clashdavid527@gmail.com>
2025-08-07 11:28:22 -0300

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatToolbarModule, MatSidenavModule, MatIconModule , MatListModule, MatButtonModule], 
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
