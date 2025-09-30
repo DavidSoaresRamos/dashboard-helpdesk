5dc2960b8acbd57ea7772f6fdd548607273017ed
DavidRamos <clashdavid527@gmail.com>
2025-09-30 12:15:14 -0300

    Update ticket-service.ts

--- src/app/service/ticket-service.ts
+++ src/app/service/ticket-service.ts
@@ -10,6 +10,6 @@ export class TicketService {
    constructor (private http: HttpClient) {}
 
    GetTickets():Observable<TicketInterface[]>{
-    return this.http.get<TicketInterface[]>('https://private-helpdesk-backend.onrender.com/tickets')
+    return this.http.get<TicketInterface[]>('https://api.render.com/deploy/srv-d3dug4qli9vc73d8r810?key=EV8V33vBD8k/tickets')
    }
 }
