dd871e2af1b1f4eda397c7c1b9f7db6616352f23
DavidRamos <clashdavid527@gmail.com>
2025-10-02 11:14:35 -0300

    Update request.ts

--- src/app/pages/request/request.ts
+++ src/app/pages/request/request.ts
@@ -1,36 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { MatTableDataSource } from '@angular/material/table';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
+import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-request',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule, HttpClientModule],
   templateUrl: './request.html',
   styleUrl: './request.css'
-})
+}) 
 export class Request implements OnInit {
-  displayedColumns: string[] = ['name', 'sector', 'problem', 'status', 'id', 'actions'];
-  dataSource = new MatTableDataSource<any>([]);
 
-  @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort;
-
+  public getTicketValue: any;
+  public displayedColumns: string[] = ['name', 'sector', 'problem', 'id'];
+  public dataSource: any = [];
   constructor(private http: HttpClient) {}
-
   ngOnInit(): void {
-    this.http.get<any[]>('https://private-helpdesk-backend.onrender.com/tickets')
-      .subscribe(tickets => {
-        this.dataSource.data = tickets;
-        this.dataSource.paginator = this.paginator;
-        this.dataSource.sort = this.sort;
-      });
+    this.getMethod();
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+  public getMethod() {
+    ticket: this.getTicketValue;
+    this.http.get('https://private-helpdesk-backend.onrender.com/tickets').subscribe((ticket) => {
+      console.log(ticket);
+      this.getTicketValue = ticket;
+      this.dataSource = ticket;
+    });
   }
 }
