fe00d0e7dc29430297c14f77cbf370bc4de1dd6b
DavidSoaresRamos <clashdavid527@gmail.com>
2025-12-02 15:23:45 -0300

    .

---
+++ src/app/interface/card-interface.ts
@@ -0,0 +1,8 @@
+export interface CardInterface {
+    
+    name: string;
+    ddd: number;
+    number: number;
+    observation: string;
+    
+}
--- src/app/pages/chips/chips.ts
+++ src/app/pages/chips/chips.ts
@@ -1,12 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpClientModule, HttpErrorResponse } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon'; 
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; 
+
 
 
 @Component({
   selector: 'app-chips',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatTableModule,
+    MatIconModule, 
+    HttpClientModule,
+  ], 
   templateUrl: './chips.html',
   styleUrl: './chips.css'
 })
-export class Chips {
 
+export class Chips implements OnInit {
+private readonly API_URL = 'https://private-helpdesk-backend.onrender.com/SIMCard';
+  public displayedColumns: string[] = [ 'name', 'ddd', 'number', 'observation' ];
+  public dataSource: any[] = [];
+  public getChipsValue: any;
+  constructor(private http: HttpClient) {}
+
+ ngOnInit(): void {
+    this.getMethod();
+  }
+
+  public getMethod() {
+    this.http.get<any[]>(this.API_URL).subscribe({
+      next: (chips) => {
+        this.getChipsValue = chips;
+        this.dataSource = chips;
+      },
+      error: (err) => console.error('Erro ao buscar Cartões SIM:', err)
+    });
+  }
 }
